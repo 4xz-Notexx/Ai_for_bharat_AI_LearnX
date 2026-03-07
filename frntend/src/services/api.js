eac7bd8d17e30511743503717ab9f41248c6579b
Anas Ahmad <ahmadanas6662@gmail.com>
2026-03-07 19:53:21 +0530

    Update api.js

--- frntend/src/services/api.js
+++ frntend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://3.85.143.12:8000"
+    baseURL: "http://3.85.143.12:8000/docs"
 });
 
 export const askAI = ({ user_id, question, difficulty }) =>
@@ -21,3 +21,4 @@ export const uploadPDF = (user_id, formData) =>
 export const clearSession = (user_id) =>
 
     API.post("/clear-session", { user_id });
+
